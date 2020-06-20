6c108972407377b81b01d3e76b71dceea67bf712
LZylstra <lindseyzylstra@gmail.com>
2020-06-20 01:54:06 -0700

    initial commit

---
+++ clients.js
@@ -0,0 +1,19 @@
+var clientsList = {
+  clients: [
+    {
+      name: "10x",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10191",
+      alternatives: ["10 x"],
+    },
+    {
+      name: "hitachi",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10184",
+      alternatives: ["hitachi vantara", "hitachi vantara corporation"],
+    },
+    {
+      name: "intuitive surgical",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/8577",
+      alternatives: ["is", "isi", "intuitive"],
+    },
+  ],
+};

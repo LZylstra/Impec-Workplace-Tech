25f2bac9cc3ea61a091dcdae26c1df02feb69c67
LZylstra <lindseyzylstra@gmail.com>
2020-06-20 14:31:29 -0700

    adds functionality to input and url routing

--- clients.js
+++ clients.js
@@ -15,5 +15,101 @@ var clientsList = {
       url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/8577",
       alternatives: ["is", "isi", "intuitive"],
     },
+    {
+      name: "lam research",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/9130",
+      alternatives: ["lam"],
+    },
+    {
+      name: "liveramp",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10181",
+      alternatives: [""],
+    },
+    {
+      name: "mozilla",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10179",
+      alternatives: [""],
+    },
+    {
+      name: "pacific biosciences",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10365",
+      alternatives: ["pacbio", "pacific bio"],
+    },
+    {
+      name: "pure storage",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10185",
+      alternatives: ["pure"],
+    },
+    {
+      name: "rms",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10182",
+      alternatives: ["risk management systems"],
+    },
+    {
+      name: "robert half",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10186",
+      alternatives: ["rhi"],
+    },
+    {
+      name: "ross stores",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/9116",
+      alternatives: ["ross"],
+    },
+    {
+      name: "san mateo county",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10190",
+      alternatives: ["smc", "san mateo"],
+    },
+    {
+      name: "servicenow",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10350",
+      alternatives: ["sn"],
+    },
+    {
+      name: "snowflake",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10180",
+      alternatives: [""],
+    },
+    {
+      name: "sony",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10183",
+      alternatives: [
+        "sie",
+        "sony interactive entertainment",
+        "sony",
+        "sony playstation",
+      ],
+    },
+    {
+      name: "teamwerc",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10352",
+      alternatives: [""],
+    },
+    {
+      name: "thermo",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10187",
+      alternatives: [
+        "thermo fisher",
+        "tf cmd",
+        "tf lsg",
+        "thermo fisher cmd",
+        "thermo fisher lsg",
+      ],
+    },
+    {
+      name: "tibco",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/10351",
+      alternatives: ["tibco software"],
+    },
+    {
+      name: "waymo",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/8579",
+      alternatives: [""],
+    },
+    {
+      name: "impec group",
+      url: "https://app.limblecmms.com/#!/loc-problem/rrzag31130/7508",
+      alternatives: ["impec", "ig"],
+    },
   ],
 };
